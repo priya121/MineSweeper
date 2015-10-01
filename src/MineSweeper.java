0f6ececa9ddbf46455e2c88eab7757c1118e8efb
priya121 <priya121@github.com>
2015-10-01 18:28:52 +0100

    Removed grid variable and constructor class.

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 public class MineSweeper {
 
     private ArrayList<Integer> mineLocations = new ArrayList<>();
-    private ArrayList<String> grid = new ArrayList<>();
-
-    public MineSweeper(int size) {
-        mineLocations.add(-1);
-    }
 
     public MineSweeper(int size, ArrayList<Integer> mineLocations) {
         this.mineLocations = mineLocations;
