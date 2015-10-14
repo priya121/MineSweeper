4a4adf3d887053ef4ac5ebce2eb7dc9f230b36b3
priya121 <priya121@github.com>
2015-10-14 11:26:05 +0100

    Any size board with duplication in tests

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -2,14 +2,141 @@ import java.util.ArrayList;
 
 public class MineSweeper {
 
+    private final int size;
     private ArrayList<Integer> mineLocations = new ArrayList<>();
+    private ArrayList<Integer> numbersAroundMines = new ArrayList<>();
+    int state = 0;
+    ArrayList<Integer> neighbours = new ArrayList<>();
 
     public MineSweeper(int size, ArrayList<Integer> mineLocations) {
+        this.size = size;
         this.mineLocations = mineLocations;
     }
 
-    public boolean checkEmpty(int location) {
+    public boolean isEmpty(int location) {
         return !mineLocations.contains(location);
     }
-}
 
+    public ArrayList<Integer> indicesAroundMine() {
+        ArrayList<Integer> indicesAroundIndividualMine = new ArrayList<>();
+
+        for (int i = 0; i < mineLocations.size(); i++) {
+            if (notTopRowOrFirstColumn(mineLocations.get(i))) {
+                indicesAroundIndividualMine.add(mineLocations.get(i) - 3);
+            }
+            if (notTopRow(mineLocations.get(i))) {
+                indicesAroundIndividualMine.add(mineLocations.get(i) - 2);
+            }
+
+            if (notFirstColumn(mineLocations.get(i))) {
+                indicesAroundIndividualMine.add(mineLocations.get(i) - 1);
+            }
+
+            if (notLastColumn(mineLocations.get(i))) {
+                indicesAroundIndividualMine.add(mineLocations.get(i) + 1);
+            }
+
+            if (notBottomRow(mineLocations.get(i))) {
+                indicesAroundIndividualMine.add(mineLocations.get(i) + 2);
+            }
+
+            if (notBottomRowLastColumn(mineLocations.get(i))) {
+                indicesAroundIndividualMine.add(mineLocations.get(i) + 3);
+            }
+        }
+        return indicesAroundIndividualMine;
+    }
+
+    public ArrayList<Integer> removeIndicesWithMines(int size, ArrayList<Integer> mineLocations) {
+        for (Integer mineLocation : mineLocations) {
+            if (isEmpty(mineLocation)) {
+                numbersAroundMines.add(mineLocation);
+            }
+        }
+        return numbersAroundMines;
+    }
+
+    public int numberOfMinesAround(int location) {
+        if (!isEmpty(location - 1) || !isEmpty(location - 3) || !isEmpty(location - 4) || !isEmpty(location - 5)) {
+            state = 1;
+        } else if (!isEmpty(location) || !mineLocations.contains(location)) {
+            state = 0;
+        }
+        return state;
+    }
+
+    private boolean notBottomRowLastColumn(int mineLocationIndex) {
+        if (mineLocationIndex == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean notLastColumn(int mineLocationIndex) {
+        if (mineLocationIndex != 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean notTopRow(int mineLocationIndex) {
+        if ((mineLocationIndex >= 2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean notFirstColumn(int mineLocationIndex) {
+        if (mineLocationIndex != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean notBottomRow(int mineLocationIndex) {
+        if (mineLocationIndex <= 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean notTopRowOrFirstColumn(int mineLocationIndex) {
+        if (mineLocationIndex == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> neighbourCells(int location) {
+        int width = (int) Math.sqrt(size);
+        int height = (int) Math.sqrt(size);
+        int xLocation = ((location % width));
+        int yLocation = ((location / height));
+
+        if (xLocation > 0 && location > 0)
+            neighbours.add((location - (width) - 1));
+
+        if (yLocation > 0)
+            neighbours.add(((location - (width))));
+
+        if (xLocation < (width - 1) && yLocation > 0)
+            neighbours.add(location - (width - 1));
+
+        if (xLocation > 0)
+            neighbours.add(location - 1);
+
+        if (xLocation < (width - 1))
+            neighbours.add(location + 1);
+
+        if (xLocation > 0 && (yLocation < (height - 1)))
+        neighbours.add(location + (width - 1));
+
+        if (yLocation < (height - 1))
+        neighbours.add(location + width);
+
+        if (xLocation < width - 1 && yLocation < height - 1)
+        neighbours.add(location + (width + 1));
+
+        return neighbours;
+    }
+}
--- src/MineSweeperTest.java
+++ src/MineSweeperTest.java
@@ -1,52 +1,252 @@
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MineSweeperTest {
 
+    private ArrayList<Integer> mineLocations;
+
+    @Before
+    public void setUp() throws Exception {
+        mineLocations = new ArrayList<>();
+    }
+
     @Test
     public void oneByOneGridIsEmpty() {
-        MineSweeper game = new MineSweeper(1);
-        assertTrue(game.checkEmpty(0));
+        MineSweeper game = new MineSweeper(1, mineLocations);
+        assertTrue(game.isEmpty(0));
     }
 
     @Test
     public void oneByOneGridWithMine() {
-        ArrayList<Integer> mineLocations = new ArrayList<>();
         mineLocations.add(0);
         MineSweeper game = new MineSweeper(1, mineLocations);
-        assertFalse(game.checkEmpty(0));
+        assertFalse(game.isEmpty(0));
     }
 
     @Test
     public void twoByTwoGridWithOneMine() {
-        ArrayList<Integer> mineLocations = new ArrayList<>();
         mineLocations.add(0);
-        MineSweeper game = new MineSweeper(2,mineLocations);
-        assertFalse(game.checkEmpty(0));
+        MineSweeper game = new MineSweeper(2, mineLocations);
+        assertFalse(game.isEmpty(0));
     }
 
     @Test
     public void twoByTwoGridWithTwoMines() {
-        ArrayList<Integer> mineLocations = new ArrayList<>();
         mineLocations.add(0);
         mineLocations.add(1);
-        MineSweeper game = new MineSweeper(2,mineLocations);
-        assertFalse(game.checkEmpty(0));
-        assertFalse(game.checkEmpty(1));
+        MineSweeper game = new MineSweeper(2, mineLocations);
+        assertFalse(game.isEmpty(0));
+        assertFalse(game.isEmpty(1));
     }
 
     @Test
     public void twoByTWoGridWithTwoMinesNotAdjacent() {
-        ArrayList<Integer> mineLocations = new ArrayList<>();
         mineLocations.add(0);
         mineLocations.add(2);
-        MineSweeper game = new MineSweeper(2,mineLocations);
-        assertFalse(game.checkEmpty(0));
-        assertFalse(game.checkEmpty(2));
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        assertFalse(game.isEmpty(0));
+        assertFalse(game.isEmpty(2));
+    }
+
+    @Test
+    public void numberOfMinesAroundEmptyCell() {
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        assertEquals(0, game.numberOfMinesAround(0));
+        assertEquals(0, game.numberOfMinesAround(1));
+        assertEquals(0, game.numberOfMinesAround(2));
+        assertEquals(0, game.numberOfMinesAround(3));
+    }
+
+    @Test
+    @Ignore
+    public void numberOfMinesAroundOneMine() {
+        mineLocations.add(0);
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        assertEquals(0, game.numberOfMinesAround(0));
+        assertEquals(1, game.numberOfMinesAround(1));
+        assertEquals(1, game.numberOfMinesAround(2));
+        assertEquals(1, game.numberOfMinesAround(3));
+    }
+
+    @Test
+    @Ignore
+    public void numberOfMinesAroundOneMineAtIndex1() {
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(1);
+        assertEquals(1, game.numberOfMinesAround(0));
+        assertEquals(0, game.numberOfMinesAround(1));
+        assertEquals(1, game.numberOfMinesAround(2));
+        assertEquals(1, game.numberOfMinesAround(3));
+    }
+
+    @Test
+    public void allCoordinatesInTwoByTwoWithMine() {
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(1);
+        indicesAroundMine.add(2);
+        indicesAroundMine.add(3);
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(0);
+        assertEquals(indicesAroundMine, game.indicesAroundMine());
+    }
+
+    @Test
+    public void numbersAroundMineAtLocation1() {
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(0);
+        indicesAroundMine.add(2);
+        indicesAroundMine.add(3);
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(1);
+        assertEquals(indicesAroundMine, game.indicesAroundMine());
+    }
+
+    @Test
+    public void numbersAroundMineAtLocation2() {
+        ArrayList<Integer> mineLocations = new ArrayList<>();
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(0);
+        indicesAroundMine.add(1);
+        indicesAroundMine.add(3);
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(2);
+        assertEquals(indicesAroundMine, game.indicesAroundMine());
+    }
+
+    @Test
+    public void numbersAroundMineAtLocation3() {
+        ArrayList<Integer> mineLocations = new ArrayList<>();
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(0);
+        indicesAroundMine.add(1);
+        indicesAroundMine.add(2);
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(3);
+        assertEquals(indicesAroundMine, game.indicesAroundMine());
+    }
+
+    @Test
+    public void indicesAroundTwoMines() {
+        ArrayList<Integer> mineLocations = new ArrayList<>();
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(2);
+        indicesAroundMine.add(3);
+        indicesAroundMine.add(2);
+        indicesAroundMine.add(3);
+        mineLocations.add(0);
+        mineLocations.add(1);
+        ArrayList<Integer> indicesAroundIndividualMines = game.indicesAroundMine();
+        assertEquals(indicesAroundMine, game.removeIndicesWithMines(4, indicesAroundIndividualMines));
+    }
+
+    @Test
+    public void indicesAroundTwoMinesNotAdjacent() {
+        ArrayList<Integer> mineLocations = new ArrayList<>();
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(0);
+        indicesAroundMine.add(3);
+        indicesAroundMine.add(0);
+        indicesAroundMine.add(3);
+        mineLocations.add(1);
+        mineLocations.add(2);
+        ArrayList<Integer> indicesAroundIndividualMines = game.indicesAroundMine();
+        assertEquals(indicesAroundMine, game.removeIndicesWithMines(4, indicesAroundIndividualMines));
+    }
+
+    @Test
+    public void indicesAroundThreeMines() {
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        indicesAroundMine.add(3);
+        indicesAroundMine.add(3);
+        indicesAroundMine.add(3);
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(0);
+        mineLocations.add(1);
+        mineLocations.add(2);
+        ArrayList<Integer> indicesAroundIndividualMines = game.indicesAroundMine();
+        assertEquals(indicesAroundMine, game.removeIndicesWithMines(4, indicesAroundIndividualMines));
+    }
+
+    @Test
+    public void indicesAroundFourMines() {
+        ArrayList<Integer> indicesAroundMine = new ArrayList<>();
+        MineSweeper game = new MineSweeper(4, mineLocations);
+        mineLocations.add(0);
+        mineLocations.add(1);
+        mineLocations.add(2);
+        mineLocations.add(3);
+        ArrayList<Integer> indicesAroundIndividualMines = game.indicesAroundMine();
+        assertEquals(indicesAroundMine, game.removeIndicesWithMines(4, indicesAroundIndividualMines));
+    }
+
+    @Test
+    public void allCoordinatesForThreeByThree() {
+        MineSweeper game = new MineSweeper(9, mineLocations);
+        assertTrue(game.isEmpty(0));
+    }
+
+    @Test
+    public void threeByThreeWithAMine() {
+        MineSweeper game = new MineSweeper(9, mineLocations);
+        assertTrue(game.isEmpty(0));
+        mineLocations.add(0);
+        assertEquals(0, game.numberOfMinesAround(0));
+        assertEquals(1, game.numberOfMinesAround(1));
+        assertEquals(0, game.numberOfMinesAround(2));
+        assertEquals(1, game.numberOfMinesAround(3));
+        assertEquals(1, game.numberOfMinesAround(4));
+        assertEquals(1, game.numberOfMinesAround(5));
+        assertEquals(0, game.numberOfMinesAround(6));
+    }
+
+    @Test
+    public void calculatesNeighbouringCellsForFour() {
+        MineSweeper game = new MineSweeper(9, mineLocations);
+        assertTrue(game.isEmpty(0));
+        mineLocations.add(4);
+        ArrayList<Integer> neighbours = new ArrayList<>();
+        neighbours.add(0);
+        neighbours.add(1);
+        neighbours.add(2);
+        neighbours.add(3);
+        neighbours.add(5);
+        neighbours.add(6);
+        neighbours.add(7);
+        neighbours.add(8);
+        assertEquals(neighbours, game.neighbourCells(4));
+    }
+
+    @Test
+    public void calculatesNeighbouringCellsForFive() {
+        MineSweeper game = new MineSweeper(9, mineLocations);
+        assertTrue(game.isEmpty(0));
+        mineLocations.add(5);
+        ArrayList<Integer> neighbours = new ArrayList<>();
+        neighbours.add(1);
+        neighbours.add(2);
+        neighbours.add(4);
+        neighbours.add(7);
+        neighbours.add(8);
+        assertEquals(neighbours, game.neighbourCells(5));
+    }
+
+    @Test
+    public void calculatesNeighbouringCellsForSix() {
+        MineSweeper game = new MineSweeper(9, mineLocations);
+        assertTrue(game.isEmpty(0));
+        mineLocations.add(6);
+        ArrayList<Integer> neighbours = new ArrayList<>();
+        neighbours.add(3);
+        neighbours.add(4);
+        neighbours.add(7);
+        assertEquals(neighbours, game.neighbourCells(6));
     }
 }
 
